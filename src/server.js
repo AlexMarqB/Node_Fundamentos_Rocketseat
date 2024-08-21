e4fc4d9b3d3566f6e1f8b0f1157376d15ffacf4b
Alex Marques Beraldo <alexmarquesberaldo@gmail.com>
2024-08-21 09:44:47 -0300

    Entendendo rotas HTTP - GET, POST, PUT, PATCH, DELETE

--- src/server.js
+++ src/server.js
@@ -7,9 +7,32 @@
 import http from 'node:http';
 //ex: import fastify from 'fastify'
 
+//ROTAS - meios de entrada para o consumidor da API chamar as funcionalidades do nosso app
+//Requisição HTTP - Métodos: GET, POST, PUT, PATCH, DELETE (CRUD)
+
+// GET => Buscar uma recurso do backend
+// POST => Criar uma recurso no backend
+// PUT => Atualizar um recurso no backend - ex: um formulario com muitos campos
+// PATCH => Atualizar uma informação especifica de um recurso no backend - ex: atualizar apenas 1 campo
+// DELETE => Deletar um recurso no backend
+
+// Podemos ter rotas com o mesmo URL porém metódos diferentes por exemplo
+// Get /users => Busca um user
+// Post /users => Criando um user
+
 const server = http.createServer((request, response) => {
     // Criar um usuário (nome, email, senha) => o request possui todas essas informações
     // Devolver uma resposta enviamos o response
+    const {method, url} = req;
+
+    if(method ===  'GET' && url === '/users') {
+        return response.end('Listagem de usuários')
+    }
+
+    if(method ===  'POST' && url === '/users') {
+        return response.end('Criação de usuários')
+    }
+
     return response.end("Hello world")
 })
 
