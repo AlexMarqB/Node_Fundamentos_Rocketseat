8ee3a4da5bf17799d7f2844e9c97756404f08d00
Alex Marques Beraldo <alexmarquesberaldo@gmail.com>
2024-08-22 11:41:38 -0300

    Persistindo o database em um arquivo fisico em .json

--- src/database.js
+++ src/database.js
@@ -1,10 +1,25 @@
+import fs from 'node:fs/promises'
 
+//                            ../ volta 2 diretorios
+const databasePath = new URL('../db.json', import.meta.url) //import.mete.url => Aponta todo o caminho do arquivo atual
 //Mesmo banco de dados para mais informações além dos usuarios
 // { "users": [...] }
 
 export class Database {
     //com o hashtag na frente da prop ela se torna privada
     #database = {}
+
+    constructor() {
+        fs.readFile(databasePath, 'utf-8')
+        .then(data => this.#database = JSON.parse(data))
+        .catch(() => {
+            this.#persist()
+        })
+    }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
 
     select(table) {
         //busca se existe uma chave === table e retorna todos os dados
@@ -19,6 +34,8 @@ export class Database {
         } else {
             this.#database[table] = [data]; //Cria a chave e insere o valor
         }
+
+        this.#persist();
 
         return data;
     }
